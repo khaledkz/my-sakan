1322c85092c669c9b77b1b88811f4ba69b0d8e7a
kharazzoro <kha.1991.14.2017@gmail.com>
2018-02-17 09:34:21 +0000

    add flat form

--- server/routes/flat/flat.js
+++ server/routes/flat/flat.js
@@ -9,4 +9,8 @@ router.get('/edit', function(req, res, next) {
     res.render('flat-edit-list')
 })
 
+router.post('/add',(req, res, next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
  module.exports = router;
