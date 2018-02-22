a35a902decfc3787db10276933be1ec4fffd4ea2
kharazzoro <kha.1991.14.2017@gmail.com>
2018-02-22 19:44:45 +0000

    use router nav

--- client/src/container/AppCountainer.js
+++ client/src/container/AppCountainer.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 
 import Header from '../component/home/Header';
 import Footer from '../component/home/Footer';
+import Menu from '../component/menu/Menu';
 import SearchSection from '../component/home/Search'
 import './css/AppCountainer.css'
-export default class AppContainer extends Component{
-    render(){
-        return(
-            <div>
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-                <Header />
-                <SearchSection  />
-                <Footer />   
-                
-            </div>
+
+import About from '../component/cards/About';
+import Contact from '../component/cards/Contact';
+import More from '../component/cards/More';
+
+export default class AppContainer extends Component {
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Header />
+
+                    <Route exact path="/" component={SearchSection} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/more" component={More} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Footer />
+
+
+                </div>
+            </Router>
         )
     }
 }
