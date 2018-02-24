3bcc1927c2ab2f8c6309326a92a9029d981fa7ed
kharazzoro <kha.1991.14.2017@gmail.com>
2018-02-24 08:01:52 +0000

    post new account

--- server/routes/authentication/register/register.js
+++ server/routes/authentication/register/register.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router =express.Router();
+const AuthenticationDb = require('../../../db-client/account/account')
 
 router.get('/signup',(req,res,next)=>{
       res.render('add-register-form')
 })
+
+router.post('/signup',(req,res,next)=>{
+      const query = req.body;
+      cb=()=>{
+            res.redirect('/')
+      }
+      AuthenticationDb.SignUp(query,cb);
+})
+
 
 module.exports= router;
