bccd09127e0c835afb039ec657c2427c7d392395
kharazzoro <kha.1991.14.2017@gmail.com>
2018-03-11 17:24:01 +0000

    redux single flat

--- client/src/container/AppCountainer.js
+++ client/src/container/AppCountainer.js
@@ -4,7 +4,7 @@ import Footer from '../component/home/Footer';
 import SearchSection from '../component/home/Search'
 import './css/AppCountainer.css'
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
-import SingleArticle from '../component/flat/SingleFlat'
+import SingleFlat from '../component/flat/SingleFlat'
 import About from '../component/cards/About';
 import Contact from '../component/cards/Contact';
 import More from '../component/cards/More';
@@ -22,7 +22,7 @@ export default class AppContainer extends Component {
                         <Route exact path="/signup" component={CreateAccount} />
                         <Route exact path="/contact" component={Contact} />
                         <Route exact path="/user-mangment" component={PrivateContainer} />
-                        <Route exact path="/flat/:flatId" component={SingleArticle} />
+                        <Route exact path="/flat/:flatId" component={SingleFlat} />
                      <Footer />
 
 
