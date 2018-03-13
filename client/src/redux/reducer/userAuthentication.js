693fe7b1d8297d31216d0fe441bb3195e2c4634a
kharazzoro <kha.1991.14.2017@gmail.com>
2018-03-13 07:29:28 +0000

    reduxstore-save-authentication

--- client/src/redux/reducer/userAuthentication.js
+++ client/src/redux/reducer/userAuthentication.js
@@ -1,11 +1,14 @@
-export default function UserAuthentication(state={},action){
+export default function UserAuthentication(state={user:{},authenticate:false},action){
     switch(action.type){
         case 'Success_Authe':
-            return Object.assign({},...state,{user:action.user});
+            return Object.assign({},...state,{user:action.user,authenticate:true});
         
        case 'Faild_Authe':
-            return Object.assign({},...state,{user:action.user});
-          
+            return Object.assign({},...state,{user:action.user,authenticate:false});
+    
+        case 'Reset_Authe':
+            return Object.assign({},...state,{user:{},authenticate:false});
+             
         default:
             return state;
     }
